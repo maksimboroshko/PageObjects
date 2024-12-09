7f194eda6b5983d53e0dbe7259dda710e13127f7
Maksim Boroshko <maksimboroshko211@gmail.com>
2024-12-09 22:54:07 +0300

    fixed

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,20 +5,21 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import pages.RegistrationPage;
 
+
 public class TestBase {
     RegistrationPage registrationPage;
     @BeforeAll
     static void setup() {
         Configuration.baseUrl = "https://demoqa.com/";
         Configuration.browser = "chrome"; // Укажите браузер
-        Configuration.headless = true; // Headless режим для сервера Jenkins
+        Configuration.headless = false; // Headless режим для сервера Jenkins
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager"; // Можно также попробовать "normal"
         Configuration.timeout = 5000;
     }
 
     @BeforeEach
-     public void preparation() {
+    public void preparation() {
         registrationPage = new RegistrationPage();
         registrationPage.openPage().removeBanners();
     }
