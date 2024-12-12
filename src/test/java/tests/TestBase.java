467f6ea1c61035c6c3609970161a20d4c78917b5
Maksim Boroshko <maksimboroshko211@gmail.com>
2024-12-12 15:55:49 +0300

    Обновление логики тестов

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -16,9 +16,13 @@ public class TestBase {
     RegistrationPage registrationPage;
     @BeforeAll
     static void setup() {
+        Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.browser = "chrome"; // Укажите браузер
         Configuration.headless = true; // Headless режим для сервера Jenkins
+        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager"; // Можно также попробовать "normal"
         Configuration.timeout = 5000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
