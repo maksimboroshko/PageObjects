83a20314ef9ae08b1ae24fe199f8e7aed0ab3e8a
Maksim Boroshko <maksimboroshko211@gmail.com>
2024-12-20 18:11:56 +0300

    fixed

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,7 +17,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-            Configuration.baseUrl = "https://demoqa.com/";
+            Configuration.baseUrl = "https://demoqa.com";
             Configuration.pageLoadStrategy = "eager";
             Configuration.browser = System.getProperty("browser", "chrome");
             Configuration.browserVersion = System.getProperty("browserVersion");
